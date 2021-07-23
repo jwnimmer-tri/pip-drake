f8c24434966e47efa13de2e5722a5ba62f10b61e
Matthew Woehlke <matthew.woehlke@kitware.com>
2021-07-23 13:21:14 -0400

    Fix libtiff's use of our lzma (xz)

--- projects/libtiff.cmake
+++ projects/libtiff.cmake
@@ -1,5 +1,5 @@
 ExternalProject_Add(libtiff
-    DEPENDS zlib libjpeg-turbo
+    DEPENDS zlib xz libjpeg-turbo
     URL ${libtiff_url}
     URL_MD5 ${libtiff_md5}
     ${COMMON_EP_ARGS}
@@ -8,6 +8,8 @@ ExternalProject_Add(libtiff
         ${COMMON_CMAKE_ARGS}
         -DJPEG_INCLUDE=${CMAKE_INSTALL_PREFIX}/include
         -DJPEG_LIBRARY=${CMAKE_INSTALL_PREFIX}/lib/libjpeg.a
+        -DLIBLZMA_INCLUDE=${CMAKE_INSTALL_PREFIX}/include
+        -DLIBLZMA_LIBRARY=${CMAKE_INSTALL_PREFIX}/lib/liblzma.a
         -DZLIB_INCLUDE_DIR=${CMAKE_INSTALL_PREFIX}/include
         -DZLIB_LIBRARY_DEBUG=${CMAKE_INSTALL_PREFIX}/lib/libz.a
         -DZLIB_LIBRARY_RELEASE=${CMAKE_INSTALL_PREFIX}/lib/libz.a
