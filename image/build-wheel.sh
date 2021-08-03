b6a3216496b2738d35ea6f219cd40afb5c938c76
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-08-02 19:17:23 -0700

    Add build script to extract a wheel-ish tarball

--- image/build-wheel.sh
+++ image/build-wheel.sh
@@ -13,3 +13,13 @@ bazel run \
     --define NO_IPOPT=ON \
     --define NO_DREAL=ON \
     //:install -- /opt/drake
+
+cd /opt && \
+    tar --create --gzip --file drakewhl-3.7.tar.gz -- \
+    drake-dependencies/lib \
+    drake/lib/libdrake*.so \
+    drake/lib/python*/site-packages/pydrake \
+    drake/share/doc \
+    drake/share/drake/.drake-find_resource-sentinel \
+    drake/share/drake/examples \
+    drake/share/drake/manipulation
