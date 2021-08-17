34a1fd5f66c91119186941425f214e0e5b013ce4
Matthew Woehlke <matthew.woehlke@kitware.com>
2021-08-17 14:21:08 -0400

    Add driver script to create wheel(s)
    Create a script to run Docker to generate the Drake wheel(s) and copy
    the result to the local file system. For now, this only generates one
    wheel, but eventually we expect it will generate multiple wheels for
    different platforms and/or Python versions.
    
    Co-authored-by: Jeremy Nimmer <jeremy.nimmer@tri.global>

---
+++ build-wheels.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This shell script uses Docker to build the PyPI wheel and automatically
+# extracts the generated wheel to the current directory.
+
+set -e
+
+salt=$(dd if=/dev/urandom bs=2 count=4 | od -An -x | tr -d ' ')
+id=pip-drake:$(date -u +%Y%m%d%H%M%S)-$salt
+
+###############################################################################
+
+build()
+{
+    local id=$1
+    shift 1
+
+    # Remove --force-rm if you need to inspect artifacts of a failed build
+    docker build --force-rm --tag $id "$@" "$(dirname "${BASH_SOURCE}")"
+    trap "docker image rm $id" EXIT
+}
+
+extract()
+{
+    docker run --rm $1 \
+        bash -c 'tar -cf - /wheel/dist/*.whl' | \
+        tar --strip-components=2 -xf -
+}
+
+###############################################################################
+
+# Build wheel
+build $id
+extract $id
